48ceab4ffdb73b331469abda54675632d658ba2b
rg063 <rg063@fake.bit>
2017-10-27 22:15:37 +0900

    データベース作成関数

--- realmTest/List.swift
+++ realmTest/List.swift
@@ -10,8 +10,29 @@ import UIKit
 import RealmSwift
 
 // Use them like regular Swift objects
-class VegetableList: Object {
+class FoodList: Object {
     @objc dynamic var name = ""
-    @objc dynamic var registDate  = 0
-    @objc dynamic var expireDate  = 0
+    @objc dynamic var photo = ""
+    @objc dynamic var registDate  = NSDate()
+    @objc dynamic var expireDate  = NSDate()
+    
+    // プライマリーキーの設定
+    override static func primaryKey() -> String? {
+        return "name"
+    }
+    
+    func setval(products:[String]) {
+        self.name = products[0]
+        self.photo = products[1]
+        self.registDate = daychange(datas: products[2])
+        self.expireDate = daychange(datas: products[3])
+    }
+
+    func daychange(datas: String) -> NSDate {
+        let formatter :DateFormatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyyMMdd"
+        
+        return formatter.date(from: datas)! as NSDate
+    }
 }
--- realmTest/ViewController.swift
+++ realmTest/ViewController.swift
@@ -11,27 +11,26 @@ import RealmSwift
 
 class ViewController: UIViewController {
 
+    let calendar = Calendar(identifier: .gregorian)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    @IBAction func Database(_ sender: UIButton) {
-
-        let tomato = VegetableList()
-        tomato.name = "TOMATO"
-        tomato.registDate = 20171027
-        tomato.expireDate = 20171102
-        print("name of vegetable: \(tomato.name)")
-        
-        // Get the default Realm
         let realm = try! Realm()
-        // print(Realm.Configuration.defaultConfiguration.fileURL!)
-        
-        try! realm.write {
-            realm.add(tomato)
+        encodefoods(realm: realm)
+    }
+
+    let products: [[String]] = [["TOMATO", "img0", "20171010", "20180101"], ["NEGI", "img1", "20171021", "20171121"]]
+    func encodefoods(realm: Realm) {
+        for food in products {
+            let test = FoodList()
+            test.setval(products: food)
+            try! realm.write {
+                realm.add(test)
+            }
+            print(Realm.Configuration.defaultConfiguration.fileURL!)
         }
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
