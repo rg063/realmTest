b5b4106248e8b0bbe82b76359e93de2420af3d13
rg063 <rg063@fake.bit>
2017-10-30 01:41:15 +0900

    新しい関数の追加
    日数を引数にとり、今日の日付から日数分後の日付を格納

--- realmTest/List.swift
+++ realmTest/List.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import RealmSwift
+let calendar:Calendar = Calendar(identifier: .gregorian)
 
 // Use them like regular Swift objects
 class FoodList: Object {
     @objc dynamic var name = ""
     @objc dynamic var photo = ""
-    @objc dynamic var registDate  = NSDate()
-    @objc dynamic var expireDate  = NSDate()
+    @objc dynamic var registDate  = Date()
+    @objc dynamic var expireDate  = Date()
     
     // プライマリーキーの設定
     override static func primaryKey() -> String? {
@@ -28,11 +29,62 @@ class FoodList: Object {
         self.expireDate = daychange(datas: products[3])
     }
 
-    func daychange(datas: String) -> NSDate {
+    func daychange(datas: String) -> Date {
         let formatter :DateFormatter = DateFormatter()
         
         formatter.dateFormat = "yyyyMMdd"
         
-        return formatter.date(from: datas)! as NSDate
+        return formatter.date(from: datas)! as Date
+    }
+}
+
+class VegetableList: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var photo = ""
+    @objc dynamic var registDate  = Date()
+    @objc dynamic var expireDate  = Date()
+    
+    // プライマリーキーの設定
+    override static func primaryKey() -> String? {
+        return "name"
+    }
+    
+    func setval(products:[String]) {
+        self.name = products[0]
+        self.photo = products[1]
+        self.registDate = getNow()
+        self.expireDate = deadLine(datas: Int(products[2])!)
+    }
+    func getNow() -> Date {
+        let now = Date()
+        let formatter :DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        let dateString = formatter.string(from: now)
+        return formatter.date(from: dateString)!
+    }
+    func deadLine(datas: Int) -> Date {
+        let date = Date()
+        let deadline = calendar.date(byAdding: .day, value: datas, to: calendar.startOfDay(for: date))
+        let formatter :DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        let dateString = formatter.string(from: deadline!)
+        return formatter.date(from: dateString)!
+    }
+}
+
+class VegetableDatabase: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var photo = ""
+    @objc dynamic var remainingDays  = ""
+    
+    // プライマリーキーの設定
+    override static func primaryKey() -> String? {
+        return "name"
+    }
+    
+    func setval(products:[String]) {
+        self.name = products[0]
+        self.photo = products[1]
+        self.remainingDays = products[2]
     }
 }
--- realmTest/ViewController.swift
+++ realmTest/ViewController.swift
@@ -17,18 +17,114 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let realm = try! Realm()
-        encodefoods(realm: realm)
+
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        makeVegetableDB(realm: realm, DatabaseName: vegetable)
+        makeVegetableList(realm: realm)
     }
 
-    let products: [[String]] = [["TOMATO", "img0", "20171010", "20180101"], ["NEGI", "img1", "20171021", "20171121"]]
-    func encodefoods(realm: Realm) {
-        for food in products {
-            let test = FoodList()
-            test.setval(products: food)
+    let vegetable: [[String]] = [["あしたば", "ashitaba", "1"],
+                                 ["アスパラガス", "asparagus", "1"],
+                                 ["アーティチョーク", "artichoke", "1"],
+                                 ["うど", "udo", "1"],
+                                 ["うり", "shirouri", "1"],
+                                 ["エシャロット", "shallot", "1"],
+                                 ["枝豆", "edamame", "1"],
+                                 ["えのきたけ", "enoki", "1"],
+                                 ["エリンギ", "eryngii", "1"],
+                                 ["おかひじき", "okahijiki", "1"],
+                                 ["オクラ", "okra", "1"],
+                                 ["かぶ", "kabu", "1"],
+                                 ["かぼちゃ", "pumpkin", "1"],
+                                 ["からし菜", "karashina", "1"],
+                                 ["カリフラワー", "cauliflower", "1"],
+                                 ["きくらげ", "kikurage", "1"],
+                                 ["キャベツ", "cabbage", "1"],
+                                 ["きゅうり", "cucumber", "1"],
+                                 ["空芯菜", "kuushinsai", "1"],
+                                 ["グリーンピース", "miendou", "1"],
+                                 ["クレソン", "watercress", "1"],
+                                 ["くわい", "kuwai", "1"],
+                                 ["ごぼう", "gobou", "1"],
+                                 ["小松菜", "komatsuna", "1"],
+                                 ["ゴーヤ", "goya", "1"],
+                                 ["コールラビ", "kohlrabi", "1"],
+                                 ["さつまいも", "satsumaimo", "1"],
+                                 ["さといも", "satoimo", "1"],
+                                 ["さやいんげん", "sayaingen", "1"],
+                                 ["さやえんどう", "sayaendou", "1"],
+                                 ["しいたけ", "shiitake", "1"],
+                                 ["ししとう", "shishitou", "1"],
+                                 ["シソ", "shiso", "1"],
+                                 ["しめじ", "shimeji", "1"],
+                                 ["じゃがいも", "potato", "1"],
+                                 ["春菊", "shungiku", "1"],
+                                 ["しょうが", "ginger", "1"],
+                                 ["食用菊", "shokuyoukiku", "1"],
+                                 ["ズッキーニ", "zucchini", "1"],
+                                 ["スプラウト", "sprout", "1"],
+                                 ["セロリ", "celery", "1"],
+                                 ["そらまめ", "soramame", "1"],
+                                 ["タアサイ", "tacai", "1"],
+                                 ["大根", "daikon", "1"],
+                                 ["たけのこ", "takenoko", "1"],
+                                 ["玉ねぎ", "onion", "1"],
+                                 ["チコリー", "chicory", "1"],
+                                 ["チンゲンサイ", "chingensai", "1"],
+                                 ["つるむらさき", "tsurumurasaki", "1"],
+                                 ["とうがらし", "chilipepper", "1"],
+                                 ["冬瓜", "tougan", "1"],
+                                 ["とうもろこし", "sweetcorn", "1"],
+                                 ["トマト", "tomato", "1"],
+                                 ["トレビス", "trevise", "1"],
+                                 ["なす", "eggplant", "1"],
+                                 ["なばな", "nabanakuki", "1"],
+                                 ["なめこ", "nameko", "1"],
+                                 ["にら", "nira", "1"],
+                                 ["にんじん", "carrot", "1"],
+                                 ["にんにく", "garlic", "1"],
+                                 ["ねぎ", "negi", "1"],
+                                 ["白菜", "chinesecabbage", "1"],
+                                 ["パセリ", "parsley", "1"],
+                                 ["ひらたけ", "hiratake", "1"],
+                                 ["ビーツ", "beet", "1"],
+                                 ["ピーマン", "bellpepper", "1"],
+                                 ["ふき", "fuki", "1"],
+                                 ["ふだん草", "chard", "1"],
+                                 ["ブロッコリー", "broccoli", "1"],
+                                 ["ほうれん草", "spinach", "1"],
+                                 ["まいたけ", "maitake", "1"],
+                                 ["マッシュルーム", "mushroom", "1"],
+                                 ["松茸", "matsutake", "1"],
+                                 ["みずな", "mizuna", "1"],
+                                 ["みつば", "mitsuba", "1"],
+                                 ["みょうが", "hanamyouga", "1"],
+                                 ["もやし", "moyashi", "1"],
+                                 ["モロヘイヤ", "moroheiya", "1"],
+                                 ["やまのいも", "yamanoimo", "1"],
+                                 ["ヤーコン", "yacon", "1"],
+                                 ["ゆり根", "yurine", "1"],
+                                 ["らっきょう", "rakkyo", "1"],
+                                 ["ルッコラ", "rocket", "1"],
+                                 ["ルバーブ", "rhubarb", "1"],
+                                 ["レタス", "lettuce", "1"],
+                                 ["れんこん", "renkon", "1"]]
+
+    let test: [String] = ["トマトリゾット", "TomatoRisotto", "3"]
+    func makeVegetableDB(realm: Realm, DatabaseName: [[String]]) {
+        for nood in DatabaseName {
+            let item = VegetableDatabase()
+            item.setval(products: nood)
             try! realm.write {
-                realm.add(test)
+                realm.add(item)
             }
-            print(Realm.Configuration.defaultConfiguration.fileURL!)
+        }
+    }
+    func makeVegetableList(realm: Realm) {
+        let item = VegetableList()
+        item.setval(products: test)
+        try! realm.write {
+            realm.add(item)
         }
     }
     
